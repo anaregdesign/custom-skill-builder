d41b542835bba8fdcd0480f598fb6afd8fabee5b
piroyoung <piroyoung@users.noreply.github.com>
2023-05-26 20:10:25 +0900

    fix: example

--- example/main.go
+++ example/main.go
@@ -20,10 +20,24 @@ func main() {
 		}
 		return &model.StringData{Output: strings.ToUpper(d.Input)}, nil
 	})
+	splitSkill := skill.NewSkill(func(d *model.StringData) (*model.CollectionStringData, error) {
+		if d.Input == "" {
+			return nil, model.ErrInputNotFound
+		}
+		return &model.CollectionStringData{Output: strings.Split(d.Input, " ")}, nil
+	})
+	countWordsSkill := skill.NewSkill(func(d *model.StringData) (*model.IntData, error) {
+		if d.Input == "" {
+			return nil, model.ErrInputNotFound
+		}
+		return &model.IntData{Output: len(strings.Split(d.Input, " "))}, nil
+	})
 
 	book := skill.NewBook()
 	book.Register("lower", lowerSkill.Flatten())
 	book.Register("upper", upperSkill.Flatten())
+	book.Register("split", splitSkill.Flatten())
+	book.Register("countWords", countWordsSkill.Flatten())
 
 	svc := service.NewCustomSkillService(book)
 	svc.Run()
--- model/model.go
+++ model/model.go
@@ -10,3 +10,38 @@ type StringData struct {
 	Input  string `json:"input,omitempty"`
 	Output string `json:"output,omitempty"`
 }
+
+type IntData struct {
+	Input  int `json:"input,omitempty"`
+	Output int `json:"output,omitempty"`
+}
+
+type FloatData struct {
+	Input  float64 `json:"input,omitempty"`
+	Output float64 `json:"output,omitempty"`
+}
+
+type BoolData struct {
+	Input  bool `json:"input,omitempty"`
+	Output bool `json:"output,omitempty"`
+}
+
+type CollectionStringData struct {
+	Input  []string `json:"input,omitempty"`
+	Output []string `json:"output,omitempty"`
+}
+
+type CollectionIntData struct {
+	Input  []int `json:"input,omitempty"`
+	Output []int `json:"output,omitempty"`
+}
+
+type CollectionFloatData struct {
+	Input  []float64 `json:"input,omitempty"`
+	Output []float64 `json:"output,omitempty"`
+}
+
+type CollectionBoolData struct {
+	Input  []bool `json:"input,omitempty"`
+	Output []bool `json:"output,omitempty"`
+}
